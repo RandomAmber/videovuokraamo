a89d226f4bb38128c491eba6368a41af870fb750
victoria tsvetkova <randambery@gmail.com>
2024-02-13 09:54:33 +0200

    db fixed

--- database.php
+++ database.php
@@ -1,12 +1,13 @@
 <?php
+require_once 'config.php';
 class Database
 
 {
-    // db info
-    private static $dbName = 'Amber';
-    private static $dbHost = 'localhost';
-    private static $dbUsername = 'Amber';
-    private static $dbUserPassword = 'fencys-qeGvus-zegfo4';
+    // db info no longer needed (because config.php)
+    // private static $dbName = 'Amber';
+    // private static $dbHost = 'localhost';
+    // private static $dbUsername = 'Amber';
+    // private static $dbUserPassword = 'fencys-qeGvus-zegfo4';
 
     private static $cont = null;
 
@@ -20,8 +21,9 @@ class Database
         {
             try {
                 // a connection to the database is created via the PDO interface
-                self::$cont = new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername,
-                self::$dbUserPassword);
+                // self::$cont = new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername,
+                // self::$dbUserPassword);
+                self::$cont = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USERNAME, DB_PASSWORD);
             }
             catch(PDOException $e)
             {
